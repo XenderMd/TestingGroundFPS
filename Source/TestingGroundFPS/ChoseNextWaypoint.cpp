3af8d6357c460352bbda1eb880518a8249d54c16
XenderMd <denis.stavilamd@gmail.com>
2018-12-18 15:15:00 +0100

    - Created the PatrollingGuard CPP class and the ThirdPerson BP now inherits from it. - Added Patrol Points TArray in CPP and made them available in UE Editor

--- Source/TestingGroundFPS/ChoseNextWaypoint.cpp
+++ Source/TestingGroundFPS/ChoseNextWaypoint.cpp
@@ -1,9 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChoseNextWaypoint.h"
+#include"BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChoseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	UE_LOG (LogTemp, Warning, TEXT("ChoseNextWaypoint executed !"))
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index is %i"), Index)
+
 	return EBTNodeResult::Succeeded;
 }
