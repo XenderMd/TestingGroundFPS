2a06643a3295ee2fc49f7ff13ea252ac8eb64c60
XenderMd <denis.stavilamd@gmail.com>
2018-12-19 13:04:41 +0100

    - Moved the ChoseNextWaypoint function from BP to C++

--- Source/TestingGroundFPS/ChoseNextWaypoint.cpp
+++ Source/TestingGroundFPS/ChoseNextWaypoint.cpp
@@ -2,13 +2,32 @@
 
 #include "ChoseNextWaypoint.h"
 #include"BehaviorTree/BlackboardComponent.h"
+#include "PatrollingGuard.h"
+#include "AIController.h"
 
 EBTNodeResult::Type UChoseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+	// TODO - add protection in case of an empty patrpoints array
+
+	AAIController *AIOwner = OwnerComp.GetAIOwner();
+	APawn *ControlledPawn = AIOwner->GetPawn();
+	auto *PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+
+	//Get patrol points
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint index is %i"), Index)
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
 
+	//Set next waypoint
+
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle Index
+	Index++;
+	Index = Index % (PatrolPoints.Num());
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, Index);
+
+
+	//finish execute
 	return EBTNodeResult::Succeeded;
 }
