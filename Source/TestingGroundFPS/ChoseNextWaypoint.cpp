7a21e234ee6e0ee19d5d2ec773313f6279ae9b25
XenderMd <denis.stavilamd@gmail.com>
2018-12-19 14:10:46 +0100

    - Moved to component architecture.    Now the PatrolRoute has become a standalone compnent, added separately to the ThirdPerson Charachter - the Third Person Character was reparented to the vanilla template

--- Source/TestingGroundFPS/ChoseNextWaypoint.cpp
+++ Source/TestingGroundFPS/ChoseNextWaypoint.cpp
@@ -2,24 +2,35 @@
 
 #include "ChoseNextWaypoint.h"
 #include"BehaviorTree/BlackboardComponent.h"
-#include "PatrollingGuard.h"
+#include"TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "PatrolRoute.h"
 #include "AIController.h"
 
 EBTNodeResult::Type UChoseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+	// TODO - protect against no Patrol Route component
 	// TODO - add protection in case of an empty patrpoints array
 
 	AAIController *AIOwner = OwnerComp.GetAIOwner();
 	APawn *ControlledPawn = AIOwner->GetPawn();
-	auto *PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto *PatrollingGuard = Cast<ATP_ThirdPersonCharacter>(ControlledPawn);
 
-	//Get patrol points
+	//Get patrol route
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto PatrolRoute = Cast<UPatrolRoute>(PatrollingGuard->GetComponentByClass(UPatrolRoute::StaticClass()));
+
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	//Get patrol points
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points !"))
+		return EBTNodeResult::Failed;
+	}
 
 	//Set next waypoint
-
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
 
 	//Cycle Index
