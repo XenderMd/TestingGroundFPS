d41ef2a61419b95f4e27fde2d4e8506d029d54ec
XenderMd <denis.stavilamd@gmail.com>
2019-01-27 11:16:24 +0100

    - Fixed CDO errors

--- Source/TestingGroundFPS/TestingGroundFPSGameMode.cpp
+++ Source/TestingGroundFPS/TestingGroundFPSGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundFPSGameMode::ATestingGroundFPSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacterBP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
