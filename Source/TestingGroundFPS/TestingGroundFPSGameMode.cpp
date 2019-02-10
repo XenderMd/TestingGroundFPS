10ad97a7b86d1d0a0dbf79166374c52d23e86cb9
XenderMd <denis.stavilamd@gmail.com>
2019-02-10 19:55:05 +0100

    - Bug fixes in the animation -> the aim angle is set again in the TPAnimations. - Created a new Firing component -> one for the TP and one for the FP meshes. At runtime, based on whether the pawn is used by the NPC_AI controller or by the FP, a different Firing Component will be used.  This is done to make the projectile spawning position more accurate/realistic. - Both the FP and TP Cameras are now concurently updated when the Pawn is used by the NPC_AI_BP

--- Source/TestingGroundFPS/TestingGroundFPSGameMode.cpp
+++ Source/TestingGroundFPS/TestingGroundFPSGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundFPSGameMode::ATestingGroundFPSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacterBP"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FPCharacterBP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
