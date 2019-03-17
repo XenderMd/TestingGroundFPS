b4ac420e69ffdf8ba15dcab72a9e8e05faf58773
XenderMd <denis.stavilamd@gmail.com>
2019-03-17 18:39:40 +0100

    - Moved from BP to C++ - Reparented the BP_Character to Manequin in CPP - Rewrote the component architecture - > now they are created as default subobjects in CPP - Moved the Fire function in CPP

---
+++ Source/TestingGroundFPS/Character/MannequinCPP.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MannequinCPP.generated.h"
+
+UCLASS()
+class TESTINGGROUNDFPS_API AMannequinCPP : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	
+	// Sets default values for this character's properties
+	AMannequinCPP();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	TSubclassOf <class AGunCPP>  GunBlueprint;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USkeletalMeshComponent* Mesh1P;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FirstPersonCameraComponent;
+
+	AGunCPP* Gun;
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/** Returns Mesh1P subobject **/
+	FORCEINLINE class USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	FORCEINLINE class UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void Fire();
+
+};
